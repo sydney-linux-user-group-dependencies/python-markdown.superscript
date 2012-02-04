015fb54cbebefecc9cba109c8c8ea187b2924f83
James Polley <jamezpolley@gmail.com>
2012-02-04 14:37:36 +1100

    Basic setup.py that installs this under markdown.extensions

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup (
+        name = 'markdown.superscript',
+        py_modules = ['markdown.extensions.superscript']
+        version = '0.9.0',
+        description = 'A Superscript Extension for Markdown',
+        author = 'Shane Graber',
+        author_email = 'sgraber@gmail.com'
+        url = 'https://github.com/sgraber/markdown.superscript',
+        classifiers = [
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.6',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Libraries :: Python Modules'
+        ],
+        requires = [
+            'markdown',
+            'markdown.extensions'
+        ],
+
+)
